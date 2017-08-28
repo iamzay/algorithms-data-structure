418a0208a820f913a34530e558cfae71324b842d
Unknown <zayhust@gmail.com>
2017-08-28 14:57:23 +0800

    添加selectionsort.js

---
+++ JavaScript/sorting/selectionsort.js
@@ -0,0 +1,23 @@
+function compare(a,b){
+  return a-b;
+}
+
+function selectionSort(arr,cmp){
+  cmp=cmp||compare;
+  for(let i=0;i<arr.length;++i){
+    /* 找出arr[i,len-1]中最小的数，和arr[i]交换*/
+    let min=arr[i];
+    let id=i;
+    for(let j=i+1;j<arr.length;++j){
+      if(cmp(min,arr[j])>0){
+        min=arr[j];
+        id=j;
+      }
+    }
+    [arr[i],arr[id]]=[arr[id],arr[i]];
+  }
+  return arr;
+}
+
+let param=[5,4,2,3,1];
+console.log(selectionSort(param));
