c84390663f215155be9d21356149d6bbc76ad4e9
Unknown <zayhust@gmail.com>
2017-09-20 18:15:58 +0800

    添加quicksort1

--- JavaScript/sorting/quicksort.js
+++ JavaScript/sorting/quicksort.js
@@ -1,44 +1,64 @@
-function compare(a, b) {
-  return a - b;
+const compare=(a,b)=>a-b;
+
+function swap(arr,a,b){
+  [arr[a],arr[b]]=[arr[b],arr[a]];
 }
 
-function partition(arr,left,right,cmp){
-  let pivot=arr[right-1];
-  let minEnd=left;
-  let maxEnd;
-  for(maxEnd=left;maxEnd<right-1;++maxEnd){
-    if(cmp(arr[maxEnd],pivot)<0){
-      [arr[maxEnd],arr[minEnd]]=[arr[minEnd],arr[maxEnd]];
-      ++minEnd;
-    }
-  }
-  [arr[minEnd],arr[right-1]]=[arr[right-1],arr[minEnd]];
-  return minEnd;
-}
-
-function _quickSort(arr,left,right,cmp){
-  if(left<right){
-    /* q是索引 */
-    let q=partition(arr,left,right,cmp);
-    _quickSort(arr,left,q,cmp);
-    _quickSort(arr,q+1,right,cmp);
-  }
-}
-
-function quickSort(arr,cmp){
-  if(!arr||!arr.length){
-    return [];
-  }
-
+function median3(arr,left,right,cmp){
   cmp=cmp||compare;
-  _quickSort(arr,0,arr.length,cmp);
+  const center=Math.floor((left+right)/2);
+  if(cmp(arr[left],arr[center])>0)
+    swap(arr,left,center);
+  if(cmp(arr[left],arr[right])>0)
+    swap(arr,left,right);
+  if(cmp(arr[center],arr[right])>0)
+    swap(arr,center,right);
+  swap(arr,center,right-1);
+  return arr[right-1];
+}
+
+function _quicksort(arr,left,right,cmp){
+  if(left>=right)
+    return;
+  if(right===left+1){
+    if(cmp(arr[left],arr[right])>0)
+      swap(arr,left,right);
+    return;
+  }
+  let pivot=median3(arr,left,right,cmp);
+  let i=left,j=right-1;
+  for(;;){
+    while(cmp(arr[++i],pivot)<0)
+      ;
+    while(cmp(arr[--j],pivot)>0)
+      ;
+    if(i<j)
+      swap(arr,i,j)
+    else
+      break;
+  }
+  swap(arr,i,right-1);
+  _quicksort(arr,left,i-1,cmp);
+  _quicksort(arr,i+1,right,cmp);
   return arr;
 }
 
-/* const arr=[5,4,1,3,2];
- * quickSort(arr);
- * console.log(arr);*/
+function quicksort(arr,cmp){
+  if(!arr||!arr.length)
+    return [];
+  cmp=cmp||compare;
+  _quicksort(arr,0,arr.length-1,cmp);
+  return arr;
+}
 
-export default quickSort;
-
-
+const cnt=10;
+const size=10;
+for(let j=0;j<cnt;++j){
+  let arr=[];
+  for(let i=0;i<size;++i){
+    arr.push(Math.floor(Math.random()*size));
+  }
+  console.log(arr);
+  console.log(quicksort(arr));
+  console.log("\n");
+}
